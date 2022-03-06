69fdd2ec0653d98c0f9f052f77d22080917c66f0
shyamsutar <shyamsutar629@gmail.com>
2022-03-06 17:37:38 +0530

    Bottom Nav created

--- app/src/main/java/gespoly/org/gespoly/MainActivity.java
+++ app/src/main/java/gespoly/org/gespoly/MainActivity.java
@@ -1,14 +1,29 @@
 package gespoly.org.gespoly;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import gespoly.org.gespoly.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        navController = Navigation.findNavController(this, R.id.frame_layout);
+
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
+
+
     }
 }
