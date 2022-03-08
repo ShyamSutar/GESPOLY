951ac50e2a2471d004bfb01d749785c5096eb420
shyamsutar <shyamsutar629@gmail.com>
2022-03-08 14:24:39 +0530

    Home Fragment

--- app/src/main/java/gespoly/org/gespoly/ui/home/HomeFragment.java
+++ app/src/main/java/gespoly/org/gespoly/ui/home/HomeFragment.java
@@ -4,20 +4,38 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import gespoly.org.gespoly.R;
+
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+
+
+
+import gespoly.org.gespoly.databinding.FragmentHomeBinding;
+
 
 
 public class HomeFragment extends Fragment {
 
+    FragmentHomeBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        binding = FragmentHomeBinding.inflate(getLayoutInflater());
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+
+
+        return view;
     }
-}
+
+
+    }
