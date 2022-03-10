6b19ce007882aa121d04bbc6b63dc173f74b6706
shyamsutar <shyamsutar629@gmail.com>
2022-03-10 21:43:30 +0530

    gmap added in homeFragment

--- app/src/main/java/gespoly/org/gespoly/ui/home/HomeFragment.java
+++ app/src/main/java/gespoly/org/gespoly/ui/home/HomeFragment.java
@@ -1,5 +1,7 @@
 package gespoly.org.gespoly.ui.home;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import gespoly.org.gespoly.R;
@@ -23,19 +26,33 @@ import gespoly.org.gespoly.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
-    FragmentHomeBinding binding;
+    private ImageView map;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        binding = FragmentHomeBinding.inflate(getLayoutInflater());
         View view =  inflater.inflate(R.layout.fragment_home, container, false);
 
+        map = view.findViewById(R.id.map);
 
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMap();
+            }
+        });
 
         return view;
     }
 
+    private void openMap() {
+        Uri uri = Uri.parse("geo:0,0?q=Sir Dr. M.S. Gosavi Polytechnic Institute Nashik");
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        intent.setPackage("com.google.android.apps.maps");
+        startActivity(intent);
 
     }
+
+
+}
