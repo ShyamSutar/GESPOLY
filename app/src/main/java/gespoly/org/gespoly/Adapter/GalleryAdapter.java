0fced2c4304233d64401bd4a23ccb9776dea6478
shyamsutar <shyamsutar629@gmail.com>
2022-03-14 22:04:08 +0530

    added zoom

---
+++ app/src/main/java/gespoly/org/gespoly/Adapter/GalleryAdapter.java
@@ -0,0 +1,75 @@
+package gespoly.org.gespoly.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import gespoly.org.gespoly.R;
+import gespoly.org.gespoly.zoomed;
+
+public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.GalleryViewAdapter> {
+
+    private Context context;
+    private List<String> images;
+
+    public GalleryAdapter(Context context, List<String> images) {
+        this.context = context;
+        this.images = images;
+    }
+
+    @NonNull
+    @Override
+    public GalleryViewAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.gallery_image,parent,false);
+        return new GalleryViewAdapter(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GalleryViewAdapter holder, int position) {
+
+        try {
+            Picasso.get().load(images.get(position)).into(holder.imageView);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        holder.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(context, zoomed.class);
+                intent.putExtra("i1", images.get(position));
+                context.startActivity(intent);
+
+
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+
+    public class GalleryViewAdapter extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+
+        public GalleryViewAdapter(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.image);
+        }
+    }
+}
--- app/src/main/java/gespoly/org/gespoly/ui/gallery/GalleryFragment.java
+++ app/src/main/java/gespoly/org/gespoly/ui/gallery/GalleryFragment.java
@@ -2,22 +2,171 @@ package gespoly.org.gespoly.ui.gallery;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import gespoly.org.gespoly.Adapter.GalleryAdapter;
 import gespoly.org.gespoly.R;
 
 
 public class GalleryFragment extends Fragment {
 
+    RecyclerView computerDepartmentRv, mechanicalDepartmentRv, electricalDepartmentRv, civilDepartmentRv;
+    GalleryAdapter adapter;
+    DatabaseReference reference;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false);
+        View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+
+
+        computerDepartmentRv = view.findViewById(R.id.computerDepartmentRv);
+        mechanicalDepartmentRv = view.findViewById(R.id.mechanicalDepartmentRv);
+        electricalDepartmentRv = view.findViewById(R.id.electricalDepartmentRv);
+        civilDepartmentRv = view.findViewById(R.id.civilDepartmentRv);
+
+        reference = FirebaseDatabase.getInstance().getReference().child("gallery");
+
+
+        getComputerDepartmentImage();
+        getMechanicalDepartmentImage();
+        getElectricalDepartmentImage();
+        getCivilDepartmentImage();
+
+
+
+        return view;
+    }
+
+    private void getMechanicalDepartmentImage() {
+        reference.child("Mechanical Department").addValueEventListener(new ValueEventListener() {
+
+            List<String> imageList = new ArrayList<>();
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot snapshot1: snapshot.getChildren()){
+                    String data = (String) snapshot1.getValue();
+                    imageList.add(data);
+                }
+
+                adapter = new GalleryAdapter(getContext(),imageList);
+                mechanicalDepartmentRv.setLayoutManager(new GridLayoutManager(getContext(), 3));
+                mechanicalDepartmentRv.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    private void getElectricalDepartmentImage() {
+        reference.child("Electrical Department").addValueEventListener(new ValueEventListener() {
+
+            List<String> imageList = new ArrayList<>();
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot snapshot1: snapshot.getChildren()){
+                    String data = (String) snapshot1.getValue();
+                    imageList.add(data);
+                }
+
+                adapter = new GalleryAdapter(getContext(),imageList);
+                electricalDepartmentRv.setLayoutManager(new GridLayoutManager(getContext(), 3));
+                electricalDepartmentRv.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    private void getCivilDepartmentImage() {
+        reference.child("Civil Department").addValueEventListener(new ValueEventListener() {
+
+            List<String> imageList = new ArrayList<>();
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot snapshot1: snapshot.getChildren()){
+                    String data = (String) snapshot1.getValue();
+                    imageList.add(data);
+                }
+
+                adapter = new GalleryAdapter(getContext(),imageList);
+                civilDepartmentRv.setLayoutManager(new GridLayoutManager(getContext(), 3));
+                civilDepartmentRv.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    private void getComputerDepartmentImage() {
+
+        reference.child("Computer Department").addValueEventListener(new ValueEventListener() {
+
+            List<String> imageList = new ArrayList<>();
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot snapshot1: snapshot.getChildren()){
+                    String data = (String) snapshot1.getValue();
+                    imageList.add(data);
+                }
+
+                adapter = new GalleryAdapter(getContext(),imageList);
+                computerDepartmentRv.setLayoutManager(new GridLayoutManager(getContext(), 3));
+                computerDepartmentRv.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
